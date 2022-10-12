75d054a07fffa8d3a12c1c0d624edad82b8eb5da
andre <d.castronunez@itsrizzoli.it>
2022-10-12 22:54:30 +0200

    feat: navbar implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdottiComponent } from './prodotti/prodotti.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdottiComponent
+    ProdottiComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
